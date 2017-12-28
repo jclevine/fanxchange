a115276e621bcdac71ddb2b51cef0a8061d97ead
Justin Levine <jclevine.receipts@gmail.com>
2017-12-28 16:43:51 -0500

    Verifying blank lines work

--- test/task_1/test_word_counter.py
+++ test/task_1/test_word_counter.py
@@ -7,7 +7,7 @@ class WordCounterTest(TestCase):
     def test_file_not_found(self):
         self.assertRaisesRegexp(IOError, '.*No such file', WordCounter, relpath(join('task_1', 'dne.txt')))
 
-    def test_counting(self):
+    def test_counting_empty_file(self):
         word_counter = WordCounter(relpath(join('task_1', 'data', 'empty_file.txt')))
         actual = word_counter.count
         self.assertEqual(0, actual)
@@ -32,7 +32,12 @@ class WordCounterTest(TestCase):
         actual = word_counter.count
         self.assertEqual(2, actual)
 
-    def test_two_words_on_one_line_with_extra_spaces_pre_and_postpended(self):
-        word_counter = WordCounter(relpath(join('task_1', 'data', 'two_words_with_spaces.txt')))
+    def test_two_words_on_one_line_with_two_words_on_second_line(self):
+        word_counter = WordCounter(relpath(join('task_1', 'data', 'two_words_two_words.txt')))
         actual = word_counter.count
-        self.assertEqual(2, actual)
+        self.assertEqual(4, actual)
+
+    def test_two_words_on_one_line_with_two_words_on_third_line_with_empty_line_between(self):
+        word_counter = WordCounter(relpath(join('task_1', 'data', 'two_words_blank_two_words.txt')))
+        actual = word_counter.count
+        self.assertEqual(4, actual)
