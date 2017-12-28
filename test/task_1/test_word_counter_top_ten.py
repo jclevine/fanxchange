ddbac13ed1b001fd0022f33a663dde1e32331ee8
Justin Levine <jclevine.receipts@gmail.com>
2017-12-28 17:06:40 -0500

    Verify top n function

--- test/task_1/test_word_counter_top_ten.py
+++ test/task_1/test_word_counter_top_ten.py
@@ -14,3 +14,13 @@ class WordCounterTopTenTest(TestCase):
         word_counter = WordCounter(relpath(join('task_1', 'data', 'one_word.txt')))
         actual = word_counter.get_top_n_words(10)
         self.assertEqual([('hello', 1)], actual)
+
+    def test_top_ten_in_two_words_blank_two_words_file(self):
+        word_counter = WordCounter(relpath(join('task_1', 'data', 'two_words_blank_two_words.txt')))
+        actual = word_counter.get_top_n_words(10)
+        self.assertEqual([('two', 2), ('with', 1), ('words', 1)], actual)
+
+    def test_top_three_in_file_with_counts_that_cut_off_least_popular_word_file(self):
+        word_counter = WordCounter(relpath(join('task_1', 'data', 'top_three.txt')))
+        actual = word_counter.get_top_n_words(3)
+        self.assertEqual([('happy', 4), ('joy', 3), ('smiles', 2)], actual)
